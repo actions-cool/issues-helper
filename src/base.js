fb0128d17fcfc515e87c01032e7dfdc030f4fc29
xrkffgg <xrkffgg@vip.qq.com>
2020-12-30 12:36:03 +0800

    add

--- src/base.js
+++ src/base.js
@@ -146,10 +146,12 @@ async function doMarkDuplicate (owner, repo, labels) {
     return false;
   }
   const issueContents = core.getInput("duplicate-command") || '/d';
-  console.log(JSON.stringify(context))
+  
   const commentId = context.comment.id;
   const commentBody = context.comment.body;
   const issueNumber = context.issue.number;
+
+  console.log(commentId, commentBody, issueNumber)
 
   const nextBody = commentBody.replace(issueContents, 'Duplicate of');
   await doUpdateComment(owner, repo, commentId, nextBody, 'replace');
