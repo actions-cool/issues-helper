5daf9bfc4341cdba43aa0da3487855f9f0a07eb7
xrkffgg <xrkffgg@vip.qq.com>
2021-01-02 22:45:03 +0800

    Update .umirc.dev.ts

--- .umirc.dev.ts
+++ .umirc.dev.ts
@@ -1,20 +1,4 @@
 export default {
   base: '/',
-  publicPath: '/',
-  navs: {
-    'zh-CN': [
-      { title: '指 南', path: '/guide' },
-      { title: '基 础', path: '/base' },
-      { title: '进 阶', path: '/advanced' },
-      { title: '更新日志', path: '/changelog' },
-      { title: 'GitHub', path: 'https://github.com/actions-cool/issues-helper' },
-    ],
-    'en-US': [
-      { title: 'Guide', path: '/en-US/guide' },
-      { title: 'Base', path: '/en-US/base' },
-      { title: 'Advanced', path: '/en-US/advanced' },
-      { title: 'Changelog', path: '/en-US/changelog' },
-      { title: 'GitHub', path: 'https://github.com/actions-cool/issues-helper' },
-    ]
-  }
+  publicPath: '/'
 };
