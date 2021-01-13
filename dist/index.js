c78b110058ec03c6df1bb7a4e400baf6b68c553b
xrkffgg <xrkffgg@vip.qq.com>
2021-01-13 09:28:33 +0800

    up

--- dist/index.js
+++ dist/index.js
@@ -8042,7 +8042,7 @@ async function doMarkDuplicate (owner, repo, labels) {
 
   const ifCommandInput = !!duplicateCommand;
 
-  if (!commentBody.includes('?') && (ifCommandInput && commentBody.startsWith(duplicateCommand) && commentBody.split(' ')[0] == duplicateCommand) || testDuplicate(commentBody)) {
+  if (!commentBody.includes('?') && ((ifCommandInput && commentBody.startsWith(duplicateCommand) && commentBody.split(' ')[0] == duplicateCommand) || testDuplicate(commentBody))) {
     if (ifCommandInput) {
       const nextBody = commentBody.replace(duplicateCommand, 'Duplicate of');
       await doUpdateComment(owner, repo, commentId, nextBody, 'replace', true);
