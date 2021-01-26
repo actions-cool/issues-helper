dcad79596468f37d9a82f206f528c6741b189ea0
xrkffgg <xrkffgg@gmail.com>
2021-01-26 15:38:25 +0800

    chore: add prettier (#45)
    * chore: add prettier
    
    * add badge
    
    * up

--- .umirc.ts
+++ .umirc.ts
@@ -6,10 +6,8 @@ const name = 'issues-helper';
 export default defineConfig({
   title: 'Issue Helper',
   mode: 'site',
-  favicon:
-    'https://avatars1.githubusercontent.com/u/73879334?s=200&v=4',
-  logo:
-    'https://avatars1.githubusercontent.com/u/73879334?s=200&v=4',
+  favicon: 'https://avatars1.githubusercontent.com/u/73879334?s=200&v=4',
+  logo: 'https://avatars1.githubusercontent.com/u/73879334?s=200&v=4',
   exportStatic: {},
   ssr: {},
   outputPath: 'docs-dist',
@@ -38,7 +36,7 @@ export default defineConfig({
       { title: 'Advanced', path: '/en-US/advanced' },
       { title: 'Changelog', path: '/en-US/changelog' },
       { title: 'GitHub', path: 'https://github.com/actions-cool/issues-helper' },
-    ]
+    ],
   },
   menus: {
     '/guide': [
