9d66be335ddca7f6a7226c0100e2f744b3d85d75
xrkffgg <xrkffgg@vip.qq.com>
2021-02-03 00:28:16 +0800

    fix: api request limit

--- dist/index.js
+++ dist/index.js
@@ -8005,7 +8005,7 @@ async function doCreateLabel(owner, repo) {
     });
     core.info(`Actions: [create-label][${name}] success!`);
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
   }
 }
 
@@ -8053,17 +8053,6 @@ async function doMarkDuplicate(owner, repo, labels) {
 
     const ifCommandInput = !!duplicateCommand;
 
-    const res = await octokit.repos.getCollaboratorPermissionLevel({
-      owner,
-      repo,
-      username: commentUser,
-    });
-    const { permission } = res.data;
-    if (!checkPermission(requirePermission, permission)) {
-      core.info(`The user ${commentUser} is not allow!`);
-      return false;
-    }
-
     if (
       !commentBody.includes('?') &&
       ((ifCommandInput &&
@@ -8071,6 +8060,18 @@ async function doMarkDuplicate(owner, repo, labels) {
         commentBody.split(' ')[0] == duplicateCommand) ||
         testDuplicate(commentBody))
     ) {
+
+      const res = await octokit.repos.getCollaboratorPermissionLevel({
+        owner,
+        repo,
+        username: commentUser,
+      });
+      const { permission } = res.data;
+      if (!checkPermission(requirePermission, permission)) {
+        core.info(`The user ${commentUser} is not allow!`);
+        return false;
+      }
+
       if (ifCommandInput) {
         const nextBody = commentBody.replace(duplicateCommand, 'Duplicate of');
         await doUpdateComment(owner, repo, commentId, nextBody, 'replace', true);
