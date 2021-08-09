db8ab69114f001eb0c37338f0ed86eecd2350d52
meteorlxy <meteor.lxy@foxmail.com>
2021-08-09 23:41:33 +0800

    refactor: extract exclude-labels array to outer scope (#75)

--- src/public.js
+++ src/public.js
@@ -60,6 +60,7 @@ async function doQueryIssues(owner, repo, labels, state, creator) {
   let issues = [];
   let issueNumbers = [];
   if (res.length) {
+    const excludeLabelsArr = dealStringToArr(excludeLabels);
     res.forEach(iss => {
       const a = bodyIncludes ? iss.body.includes(bodyIncludes) : true;
       const b = titleIncludes ? iss.title.includes(titleIncludes) : true;
@@ -69,10 +70,9 @@ async function doQueryIssues(owner, repo, labels, state, creator) {
        * You can identify pull requests by the pull_request key.
        */
       if (a && b && iss.pull_request === undefined) {
-        if (excludeLabels) {
-          const labels = dealStringToArr(excludeLabels);
+        if (excludeLabelsArr.length) {
           for (let i = 0; i < iss.labels.length; i += 1) {
-            if (labels.includes(iss.labels[i].name)) return;
+            if (excludeLabelsArr.includes(iss.labels[i].name)) return;
           }
         }
 
