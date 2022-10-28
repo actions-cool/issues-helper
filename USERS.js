12b5247afc47a122ea9329db53cdc2cc8708df33
miracles1919 <516571350@qq.com>
2022-10-28 10:44:16 +0800

    docs: add user antd-mobile (#127)

--- USERS.js
+++ USERS.js
@@ -103,6 +103,10 @@ const users = [
     url: 'https://github.com/electron/electron',
     logo: 'https://avatars.githubusercontent.com/u/13409222?s=200&v=4',
   },
+  {
+    url: 'https://github.com/ant-design/ant-design-mobile',
+    logo: 'https://gw.alipayobjects.com/zos/bmw-prod/b874caa9-4458-412a-9ac6-a61486180a62.svg',
+  },
 ];
 
 // **************************************************************************
