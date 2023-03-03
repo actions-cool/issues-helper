a75f432b77fbf0f5b16080ef45312e01e0233eac
wuxh <wxh1220@gmail.com>
2023-03-04 06:02:22 +0800

    chore: update config file

---
+++ web/.dumirc.ts
@@ -0,0 +1,42 @@
+// more config: https://d.umijs.org/config
+import { defineConfig } from 'dumi';
+
+const name = 'issues-helper';
+
+const isProdSite =
+  // 不是预览模式 同时是生产环境
+  process.env.PREVIEW !== 'true' && process.env.NODE_ENV === 'production';
+
+const logo =
+  'https://gw.alipayobjects.com/mdn/rms_f97235/afts/img/A*8xDgSL-O6O4AAAAAAAAAAAAAARQnAQ';
+
+export default defineConfig({
+  title: 'Issues Helper',
+  outputPath: '../docs-dist',
+  base: isProdSite ? `/${name}/` : '/',
+  publicPath: isProdSite ? `/${name}/` : '/',
+  locales: [
+    { id: 'en-US', name: 'English', },
+    { id: 'zh-CN', name: '中文' },
+  ],
+  favicons: [logo],
+  themeConfig: {
+    logo,
+    nav: {
+      'zh-CN': [
+        { title: '指 南', link: '/zh-CN/guide' },
+        { title: '基 础', link: '/zh-CN/base' },
+        { title: '进 阶', link: '/zh-CN/advanced' },
+        { title: '更新日志', link: '/zh-CN/changelog' },
+        { title: 'GitHub', link: 'https://github.com/actions-cool/issues-helper' },
+      ],
+      'en-US': [
+        { title: 'Guide', link: '/guide' },
+        { title: 'Base', link: '/base' },
+        { title: 'Advanced', link: '/advanced' },
+        { title: 'Changelog', link: '/changelog' },
+        { title: 'GitHub', link: 'https://github.com/actions-cool/issues-helper' },
+      ],
+    },
+  },
+});
